f61919b77bbd6fa7aa3e2f71609d6c349c919fef
ckh-0 <1771433522@qq.com>
2020-07-17 09:50:34 +0800

    Update 欧几里得算法求最大公约数.c

--- 欧几里得算法求最大公约数.c
+++ 欧几里得算法求最大公约数.c
@@ -1,1 +1,28 @@
-
+//欧几里得算法求最大公约数
+#include<stdio.h>
+int main()
+{
+	int a, b, y, r,t,m,n;
+	printf("Please input two numbers:\n");
+	scanf("%d %d", &a, &b);
+	m = a;
+	n = b;
+	if (a >= b)
+	{
+		r = a % b;
+	}
+	else
+	{
+		t = a;
+		a = b;
+		b = t;
+		r = a % b;
+	}
+	while (r)
+	{
+		a = b;
+		b = r;
+		r = a % b;
+	}
+	printf("%d和%d最大公约数是:%d", m, n, b);
+}
