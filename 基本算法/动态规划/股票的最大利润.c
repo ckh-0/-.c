e642f7e809ba739dcb16f484ecf63259db763609
ckh-0 <1771433522@qq.com>
2020-09-06 19:53:20 +0800

    Create 股票的最大利润.c

---
+++ 基本算法/动态规划/股票的最大利润.c
@@ -0,0 +1,31 @@
+/*  股票的最大利润
+	题目：假设把某股票的价格按照时间先后顺序存储在数组中，请问买卖该股票一次可能获得的最大利润是多少？
+	示例 1:
+			输入: [7,1,5,3,6,4]
+			输出: 5
+			解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+				  注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+	示例 2:
+			输入: [7,6,4,3,1]
+			输出: 0
+			解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+	限制：
+			0 <= 数组长度 <= 10^5
+*/
+#include<stdio.h>
+#include<limits.h>
+#define pricesSizes 6
+int maxProfit(int* prices, int pricesSize) {
+	int min =INT_MAX , profit=0;
+	for (int i = 0; i < pricesSizes; i++) {
+		int res = prices[i] - min;
+		profit = res > profit ? res : profit;
+		min = min < prices[i] ? min : prices[i];
+	}
+	return profit;
+}
+int main(void) {
+	int prices[pricesSizes] = { 7,1,5,3,6,4 };
+	int res = maxProfit(prices, pricesSizes);
+	printf("%d", res);
+}
